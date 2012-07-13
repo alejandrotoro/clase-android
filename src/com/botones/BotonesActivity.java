f3fc418fbdb2c866315d6a7518d69a569021c222
Alejandro Toro <alejandrotoro@espiral.co>
2012-07-12 23:18:45 -0500

    Agregar evento al botón para capturar el valor del input y mostrarlo en pantalla

--- src/com/botones/BotonesActivity.java
+++ src/com/botones/BotonesActivity.java
@@ -2,12 +2,21 @@ package com.botones;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class BotonesActivity extends Activity {
     /** Called when the activity is first created. */
+	EditText texto;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        texto = (EditText)this.findViewById(R.id.caja_txt);
+    }
+    
+    public void guardar(View text){
+    	Toast.makeText(this, "cualquier vaina "+texto.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 }

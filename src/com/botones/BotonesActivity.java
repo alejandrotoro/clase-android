4ef1cd6e07ada17d7f2ed1e7ed709dadb2332cc1
Alejandro Toro <alejandrotoro@espiral.co>
2012-07-13 23:51:18 -0500

    usando un edit text y un boton para generar un mensaje en pantalla

--- src/com/botones/BotonesActivity.java
+++ src/com/botones/BotonesActivity.java
@@ -1,21 +1,22 @@
 package com.botones;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class BotonesActivity extends Activity {
     /** Called when the activity is first created. */
-	
-	public void aboutUs(View view){
-		Intent intent = new Intent(BotonesActivity.this, AboutUs.class);
-		startActivity(intent);
-	}
-	
+	EditText texto;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        texto = (EditText)this.findViewById(R.id.text);
+    }
+    
+    public void muestraMensaje(View text){
+    	Toast.makeText(this, "cualquier vaina "+texto.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 }
